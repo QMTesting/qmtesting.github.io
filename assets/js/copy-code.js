e1bcded7c654d0bbc0b6669134434ee09e94656b
QMTesting <margaret.cams27@gmail.com>
2026-08-18 14:19:49 -0400

    Update copy-code.js

--- assets/js/copy-code.js
+++ assets/js/copy-code.js
@@ -1,31 +1,3 @@
-window.addEventListener("load", () => {
-  const blocks = document.querySelectorAll(".entry .highlight");
-
-  blocks.forEach((block) => {
-    const wrapper = document.createElement("div");
-    wrapper.className = "code-wrapper";
-
-    block.parentNode.insertBefore(wrapper, block);
-    wrapper.appendChild(block);
-
-    const button = document.createElement("button");
-    button.className = "copy-btn";
-    button.innerText = "Copy";
-
-    wrapper.insertBefore(button, block);
-
-    const code = block.querySelector("code");
-    button.addEventListener("click", () => {
-      navigator.clipboard.writeText(code.innerText).then(() => {
-        button.innerText = "Copied!";
-        setTimeout(() => {
-          button.innerText = "Copy";
-        }, 2000);
-      });
-    });
-  });
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   // Select all code blocks rendered by Markdown
   const codeBlocks = document.querySelectorAll("div.highlight pre, pre.highlight, pre > code");
