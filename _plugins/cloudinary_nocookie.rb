3bee006dcc374d99e21dcf46b331106abc29a4d4
QMTesting <margaret.cams27@gmail.com>
2026-09-01 15:18:48 -0400

    Delete _plugins directory

--- _plugins/cloudinary_nocookie.rb
+++
@@ -1,7 +0,0 @@
-module CloudinaryNoCookie
-  def nocookie(input)
-    input.gsub("https://res.cloudinary.com", "https://res.cloudinary.com")
-  end
-end
-
-Liquid::Template.register_filter(CloudinaryNoCookie)
